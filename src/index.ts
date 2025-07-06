649a945024f0b07959afb9f78a20be4f7e07c200
brodin sudswad <brodin.yai@gmail.com>
2025-07-06 21:33:15 +0700

    Refactor app initialization and add additional routes for greeting and age responses

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.get("/", () => "Hello Elysia Framework!")
+.get("/hello", () => {
+  return {message: "Hello World!"}
+})
+.get("/greet/:name", ({ params:{name} }) => {
+  return { message: `Hello, ${name}!` };
+})
+.get("/hello/:name/:age", ({ params: { name, age } }) => {
+  return { message: `Hello, ${name}! You are ${age} years old.` };
+})
+.listen(3000);
+
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
